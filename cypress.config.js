dbda5a2d9976f7f3edd2d1202637d4ed94de0080
Reinaldo <reinaldo.rossetti@outlook.com>
2023-03-27 11:23:51 -0300

    alterando as config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://192.168.18.4',
   },
   fixturesFolder: false,
   video: false,
+  reporter: 'json',
+  reporterOptions: {
+    mochaFile: 'cypress/results/my-test-output.json',
+    toConsole: true,
+  },
 })
