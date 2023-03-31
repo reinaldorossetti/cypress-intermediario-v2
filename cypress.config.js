840806d7960cf0ac6e16c89b5eced309ba53921c
Reinaldo <reinaldo.rossetti@outlook.com>
2023-03-31 20:16:36 -0300

    adicionando testes pra rodar na esteira

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://192.168.18.4',
+    baseUrl: 'http://api.openweathermap.org/data/2.5/',
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
---
+++ cypress/e2e/api/openweathermap/weatherSearch.cy.js
@@ -0,0 +1,56 @@
+describe('Pesquisa clima por cidade', () => {
+
+  it('successfully', () => {
+    cy.request({
+      method: 'GET',
+      url: '/weather?q=São+Paulo&appid=e2f1d841cc16cff49a0aa18b531d71e0',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      expect(response.status).to.equal(200)
+    })
+  })
+
+})
+
+
+describe('Pesquisa clima por ID', () => {
+
+  it('successfully', () => {
+    cy.request({
+      method: 'GET',
+      url: '/weather?id=3448439&appid=e2f1d841cc16cff49a0aa18b531d71e0',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      expect(response.status).to.equal(200)
+    })
+  })
+
+})
+
+describe('Pesquisa clima por longitude e latitude', () => {
+
+  it('successfully', () => {
+    cy.request({
+      method: 'GET',
+      url: '/weather?lat=-23.5475&lon=-46.6361&appid=e2f1d841cc16cff49a0aa18b531d71e0',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      expect(response.status).to.equal(200)
+    })
+  })
+
+})
+
+describe('Pesquisa clima por ZipCode', () => {
+
+  it('successfully', () => {
+    cy.request({
+      method: 'GET',
+      url: '/weather?zip=94040%2Cus&appid=e2f1d841cc16cff49a0aa18b531d71e0',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      expect(response.status).to.equal(200)
+    })
+  })
+
+})
